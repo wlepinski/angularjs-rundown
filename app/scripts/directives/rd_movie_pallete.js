d9c72e419c558e7c7ad873ebe9843ea1b0f88aa2
William Lepinski <william.lepinski@rga.com>
2013-07-23 15:34:00 -0300

    Version 1.2.1

--- app/scripts/directives/rd_movie_pallete.js
+++ app/scripts/directives/rd_movie_pallete.js
@@ -8,27 +8,39 @@ angular.module('angularjsRundownApp')
         movie: '='
       },
       link: function postLink(scope, element, attrs) {
+        element = $(element[0]);
         var colorThief = new ColorThief();
+        var colorSpan = null;
+        for (var i = 0; i < 9; i++) {
+          colorSpan = $('<span></span>');
+          element.append(colorSpan);
+        };
+
+        var colorSpans = element.find('span');
 
         scope.$watch('movie', function(newValue, oldValue, scope){
           if (newValue) {
+            // We need to create a new image to calculate the palette
             var image = new Image();
 
+            // When the image loads, we can grab the color information
             image.onload = function() {
+              // Grab the color palette
               var pallete = colorThief.getPalette(image);
-              var colorSpan = null;
+
+              // Calculate the width of each pallete equally
               var width = 100 / pallete.length;
 
-
-              pallete.forEach(function(color){
-                colorSpan = $('<span></span>')
+              // For each color on the palette, create a span and assign the color
+              var colorSpan = null;
+              pallete.forEach(function(color, i){
+                colorSpan = $(colorSpans.get(i))
                   .css('backgroundColor', 'rgba('+color[0]+','+color[1]+','+color[2]+', 1)')
                   .css('width', width + '%');
-
-                element.append(colorSpan);
               });
             };
 
+            // Append the image src to start the loading process
             image.src = '/image?url=' + newValue.posters.thumbnail;
           }
         });

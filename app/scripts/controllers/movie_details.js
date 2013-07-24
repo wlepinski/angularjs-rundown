6a4a32570a070077587e9908905c78529c9d37aa
William Lepinski <willsp@gmail.com>
2013-07-24 00:49:28 -0300

    Google Analytics and some JSDocs

--- app/scripts/controllers/movie_details.js
+++ app/scripts/controllers/movie_details.js
@@ -4,4 +4,10 @@ angular.module('angularjsRundownApp')
   .controller('MovieDetailsCtrl', ['$rootScope', '$scope', '$routeParams', 'rottenTomatoesApi',
     function ($rootScope, $scope, $routeParams, rottenTomatoesApi) {
       $rootScope.movieSelected = $scope.movie = rottenTomatoesApi.movieInfo($routeParams['movie_id']);
+
+      // After the load, track an event using the Google Analitycs API
+      $scope.movie.then(function(movie){
+        ga('send', 'event', 'movie', 'details', movie.title);
+      });
+
     }]);
--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -2,12 +2,20 @@
 
 angular.module('angularjsRundownApp')
   .controller('SearchCtrl', ['rottenTomatoesApi', '$scope', '$routeParams', function (rottenTomatoesApi, $scope, $routeParams) {
-    $scope.query = $routeParams.q;
+    var query = $scope.query = $routeParams.q;
+
+    // Search for movies using the query passed via URL
     $scope.results = rottenTomatoesApi.search($routeParams.q);
     $scope.results.then(function(results){
       $scope.total = results.total;
+
+      // Track event of the movie search
+      ga('send', 'event', 'movie', 'search', query);
     });
 
+    // Method to handle the result pagination.
+    // When this method is called, a new request will be made to the
+    // backend to fetch a new collection of data.
     $scope.paginate = function (currentPage) {
       $scope.results = rottenTomatoesApi.search($routeParams.q, currentPage);
     }

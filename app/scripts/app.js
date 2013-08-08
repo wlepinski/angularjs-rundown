df8f463e1d26930e7d694be9fa76035b073998ca
William Lepinski <william.lepinski@rga.com>
2013-08-08 16:17:17 -0300

    Authentication issues

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('angularjsRundownApp', [])
+angular.module('angularjsRundownApp', ['ngCookies'])
   .constant('parseApplicationId', '4kdi7WYXH9y20Lsb3EfYMLVytOttBFwjTpPcpTrO')
   .constant('parseRestApiKey', '1l8kAcGnsjceGBGUTZ951SgtxpTdjRZPI1On5YWM')
   .constant('facebookAppId', '177530099095061')
@@ -29,28 +29,33 @@ angular.module('angularjsRundownApp', [])
         });
     }
   ])
-  .run(function($rootScope, $log, facebookAppId, appSession){
-    // Assign the appSession the $rootScope.appSession
-    // This means that any modification to appSession will be propagated
-    // to the scope and to the view.
-    $rootScope.appSession = appSession;
-
+  .run(function($rootScope, $log, $cookieStore, facebookAppId, appSession){
     window.fbAsyncInit = function() {
-      // init the FB JS SDK
+      // Init the FB JS SDK
       FB.init({
         appId      : facebookAppId,
         channelUrl : '//local.kinetoscope.herokuapp.com/channel.html',
-        status     : false,
+        // Here is a tricky. The login and logout of the application is dome in a async way
+        // this means that we need to control if we have a user is authenticated between forced
+        // page reloads. This task is handled by the $cookieStore and the currentUser variable
+        // and here we're evaluating if the $cookieStore has an currentUser. If so we set the status
+        // to true to automatically fetch the login status within the Facebook API. If we don't find
+        // a cookie named currentUser on the $cookieStore means that we don't have a user logged.
+        status     : ($cookieStore.get('currentUser')) ? true : false,
         xfbml      : true
       });
 
-      // Additional initialization code such as adding Event Listeners goes here
+      // Here we're subscribing to the auth.authResponseChange to listen when the Facebook
+      // changed the authentication status.
       FB.Event.subscribe('auth.authResponseChange', function(response) {
         $log.debug('Facebook.authResponseChange event fired with %o', response);
 
-        appSession.setCurrentUser(response, function(){
-          $log.debug('Current app user setted. Applying to scope.');
-          $rootScope.$apply();
+        // We need to trigger the $apply here because the FB.Event.subscribe is not on
+        // the angular context so we need to tell to Angular something like
+        // "Hey, can you please update the DOM now?"
+        $rootScope.$apply(function(){
+          // Set the Facebook's user on the appSession
+          appSession.setCurrentUser(response);
         });
       });
     };
--- app/scripts/services/app_session.js
+++ app/scripts/services/app_session.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('angularjsRundownApp')
-  .service('appSession', function appSession($log, $rootScope, parseApi) {
+  .service('appSession', function appSession($log, $q, $cookieStore, $rootScope, parseApi) {
     // Assign the appSession the $rootScope.appSession
     // This means that any modification to appSession will be propagated
     // to the scope and to the view.
@@ -9,13 +9,24 @@ angular.module('angularjsRundownApp')
 
     var currentUser = null;
 
-    this.setCurrentUser = function(facebookData, callback) {
-      callback = callback || angular.noop;
+    /**
+     * Set the current user on the application using a Facebook authentication data.
+     *
+     * @param  {[type]}   facebookData The Facebook's authentication data.
+     * @param  {Function} callback     A callback that will be called when the authentication job is done.
+     *
+     * @return {[type]}                [description]
+     */
+    this.setCurrentUser = function(facebookData) {
+      var job = $q.defer();
 
       // If auResponse is null we need to logout the user.
       if(facebookData === null){
+        // Remove the current user and remove it from the $cookieStore.
         currentUser = null;
-        return callback(currentUser);
+        $cookieStore.remove('currentUser');
+
+        return job.resolve(currentUser);
       }
 
       var expireDate = new Date();
@@ -33,16 +44,25 @@ angular.module('angularjsRundownApp')
 
         // Call the Facebook API to get some more information about the user
         FB.api('/me', function(facebookUser) {
-          $log.debug('Facebook user fetched.');
+          $rootScope.$apply(function(){
+            $log.debug('Facebook user fetched.');
 
-          currentUser = angular.extend(facebookUser, parseUser);
-          callback(currentUser);
+            // We need to extend the Facebook's user with the parse user cause
+            // some parts of the application use information of both providers.
+            currentUser = angular.extend(facebookUser, parseUser);
+
+            // Record a cookie with the currentUser
+            $cookieStore.put('currentUser', currentUser);
+
+            // Resolve the job defer using the current user.
+            job.resolve(currentUser);
+          });
         });
       }, function(){
         $log.error(arguments);
       });
 
-      $rootScope.$apply();
+      return job.promise;
     };
 
     this.getCurrentUser = function() {
